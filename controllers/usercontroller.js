a3218207f9e9fd98341ee94d275a46ed24c30200
Aman Kumar Bairagi <amanbairagi1089@gmail.com>
2023-12-01 10:32:08 +0530

    Some bug fixes and final email added

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -4,66 +4,70 @@ const User = require("../model/userModel.js");
 const nodemailer = require("nodemailer");
 
 
-exports.registerUser = async (req,res,next)=>{
+exports.registerUser = async (req, res, next) => {
     try {
 
-   
-    
-    const newUser = new User(req.body);
-    await newUser.save();
 
-    res.send({
-        sucess : true,
-        message : "user data added sucessfully"
-    })
-   next();
-    
+
+        const newUser = new User(req.body);
+        await newUser.save();
+
+        res.send({
+            success: true,
+            message: "Thanks for contacting us , we will get back to you in 24 hours"
+        })
+        next();
+ 
     } catch (error) {
         res.send({
-            sucess : false,
-            message : error.message,
-            
-            
-        }
+            success: false,
+            message: error.message,
+        } 
         )
     }
- 
+
 }
 
 
 
-exports.sendMail = async (req,res)=>{
-    
-        let {username,email,number,mailmessage} = req.body;
-        
-try {
-    const transporter = nodemailer.createTransport({
-          service : 'gmail',
-        auth : {
-            user : process.env.EMAIL,
+exports.sendMail = async (req, res) => {
 
-            pass : process.env.PASS
+    let { username, email, number, mailmessage } = req.body;
+
+    try {
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL,
+
+                pass: process.env.PASS
+            }
+        })
+
+
+
+        const message = {
+            from: process.env.EMAIL,
+            to: "itx3dstudio@gmail.com",
+            subject: "There is a new connection here",
+            text: `Greetings from ${username},\nWhatsApp number: ${number}\nemail : ${email}\ni want to say ${mailmessage}`
         }
-      })
-    
+        const info = await transporter.sendMail(message)
+        if (info) {
 
+            res.send({
+                success: true,
+                message: "email sent sucessfully"
+            })
+        }
 
-const message = {
-     from : process.env.EMAIL,
-     to : "amanbairagi1089@gmail.com",
-     subject : "There is a new connection here ",
-     text : `Greetings from ${username},\nWhatsApp number: ${number}\nemail : ${email}\ni want to say ${mailmessage}`
-} 
-const info = await transporter.sendMail(message)
-if(info)  res.send({
-    sucess : true,
-    message : "email sent sucessfully"
-})
-
-}catch (error) {
-    return error
+    } catch (error) {
+        res.send({
+            success: false,
+            message: "Something Went wrong"
+        })
+    }
 }
-}
-        
-    
+
+
 
